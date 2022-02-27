f6f680b1c5e3a2a60c9981a43bec45bab6358270
zhaojun <jzhao8@trip.com>
2022-02-27 18:03:26 +0800

    add

--- linux/command/sed/sed_demo.sh
+++ linux/command/sed/sed_demo.sh
@@ -15,4 +15,11 @@ nl test | sed  '2,$d'
 #在第二行后面插入一行，drink tea
 nl test | sed  '2a drink tea'
 
+#修改第一个匹配daemon的字符串替换成new_daemon，只修改缓冲区的，源文件demo不变
+sed 's/daemon/new_daemon/' demo
 
+#修改所有匹配daemon的字符串替换成new_daemon，只修改缓冲区的，源文件demo不变
+sed 's/daemon/new_daemon/g' demo
+
+#修改第一个匹配daemon的字符串替换成new_daemon，修改源文件demo
+sed -i 's/daemon/new_daemon/' demo
